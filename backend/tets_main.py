a890da7fd4034a04568a1cef2888c2a1430e9f18
besatb21 <bjacaj@student.tugraz.at>
2025-11-01 12:03:56 +0100

    add test when no idle robot left

--- backend/tets_main.py
+++ backend/tets_main.py
@@ -8,7 +8,7 @@ from .main import app, get_session, AssignOrderRequest
 client = TestClient(app)
 
 
-def test_create_hero():
+def test_assign_nearest_idle_robot():
     client.get('/reset/')
     response = client.post(
         "/assignNearestIdleRobot/", json={"name": "O-1001", "source": "B"}
@@ -18,3 +18,20 @@ def test_create_hero():
     assert response.status_code == 200
     assert data["path"]
     assert data["robot_name"]
+
+def test_no_idle_robot_available():
+    client.get('/reset/')
+    # Assign all robots to make them busy
+    client.post("/assignNearestIdleRobot/", json={"name": "O-1001", "source": "B"})
+    client.post("/assignNearestIdleRobot/", json={"name": "O-1001", "source": "B"})
+
+    # Now try to assign another robot
+    response = client.post(
+        "/assignNearestIdleRobot/", json={"name": "O-1001", "source": "B"}
+    )
+    data = response.json()
+
+    assert response.status_code == 404
+    assert data["detail"] == "No idle robot available"
+
+
